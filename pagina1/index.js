42c054cc21f7196cda56e2440116db612106c1f1
Jelte Homminga <jeltehomminga@gmail.com>
2019-06-06 22:45:58 +0200

    :bug: fixes and styling

---
+++ pagina1/index.js
@@ -0,0 +1,77 @@
+//set intial state for browser navigation
+let htmlPage1 = ''
+let htmlPage2 = ''
+let indexState = { navPage: '0' }
+
+navPage = indexState.navPage
+let title = `index`
+let path = `/index.html`
+history.pushState(indexState, title, path)
+
+//navigate and fetch pages
+const navigate = (newPage, pop) => {
+  event.preventDefault()
+  if (newPage === '0') closeModal(), (navPage = 0)
+  else {
+    navPage = newPage ? newPage : navPage === '1' ? '2' : '1'
+    nextPage = navPage === '1' ? '2' : '1'
+    fetchPage(navPage)
+    toggleCancelButton(navPage)
+    !pop && doPushState(navPage)
+    document.getElementById('nav-page').innerText = nextPage
+  }
+}
+
+//add state for navigation
+const doPushState = navPage => {
+  let state = { navPage: navPage },
+    title = `pagina${navPage}`,
+    path = navPage ? `/pagina${navPage}.html` : `/index.html`
+  history.pushState(state, title, path)
+}
+
+//navigate with browser buttons
+window.addEventListener('popstate', event => {
+  return navigate(event.state.navPage, true)
+})
+
+const openModal = () => (
+  (document.getElementById('mymodal').style.display = 'block'), navigate('1')
+)
+
+const closeModal = () => {
+  document.getElementById('mymodal').style.display = 'none'
+  history.pushState(indexState, '', `/index.html`)
+}
+
+const toggleDisplay = elId => {
+  const el = document.getElementById(elId)
+  el.style.display = el.style.display === 'none' ? 'block' : 'none'
+}
+
+const toggleCancelButton = navPage => {
+  const elCancelButton = document.getElementById('cancel-button')
+  navPage === '2'
+    ? (elCancelButton.classList.add('green-button'),
+      elCancelButton.classList.remove('red-button'))
+    : (elCancelButton.classList.remove('green-button'),
+      elCancelButton.classList.add('red-button'))
+}
+
+const fetchPage = navPage => {
+  let modalPage = document.querySelector('#modalpage')
+  if (navPage === '1' && htmlPage1) modalPage.innerHTML = htmlPage1
+  if (navPage === '2' && htmlPage2) modalPage.innerHTML = htmlPage2
+  else {
+    fetch(`/pagina${navPage}.html`)
+      .then(response => response.text())
+      .then(body => {
+        modalPage.innerHTML = body
+        if (navPage === '1') {
+          htmlPage1 = body
+        } else if (navPage === '2') {
+          htmlPage2 = body
+        }
+      })
+  }
+}
